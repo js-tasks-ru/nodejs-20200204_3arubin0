73e97ded158a759df9b6022fa8e1f5682dc55db5
3arubin <3arubin@gmail.com>
2020-02-07 00:26:09 +0300

    Eslint fix

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,9 +1,9 @@
 function sum(a, b) {
-    if (typeof a === 'number' && typeof b === 'number') {
-        return a + b;
-    } else {
-        throw new TypeError();
-    }
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  } else {
+    throw new TypeError();
+  }
 }
 
 module.exports = sum;
