65701dbcfede31bb839b6e06cfa46a3ee8da93cd
3arubin <3arubin@gmail.com>
2020-02-19 00:26:23 +0300

    Task 2

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,13 +4,21 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.options = options;
+    this._chunkStr = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this._chunkStr += chunk.toString(this.options.encoding);
+    callback(null);
   }
-
   _flush(callback) {
+    const chunkArr = this._chunkStr.split(os.EOL);
+    chunkArr.forEach((item) => {
+      this.push(item);
+    });
+    this._chunkStr = '';
+    callback(null);
   }
 }
-
 module.exports = LineSplitStream;
