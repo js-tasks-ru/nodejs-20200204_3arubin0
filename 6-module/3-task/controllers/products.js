e65439f9bb7b248f262e568fb68659ac3b26217a
3arubin <3arubin@gmail.com>
2020-02-27 02:58:03 +0300

    Module 6, Task 3

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,14 @@
+const Product = require('../models/Product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  if (ctx.request.query.query) {
+    const list = await Product.find(
+        {$text: {$search: ctx.request.query.query}},
+        {score: {$meta: 'textScore'}}
+    ).sort( {score: {$meta: 'textScore'}}
+    );
+    ctx.body = {products: list};
+  } else {
+    ctx.body = {products: []};
+  }
 };
