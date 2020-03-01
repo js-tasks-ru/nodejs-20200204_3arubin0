f1e745eff4d75c3367095845dc85f6972a39ea97
3arubin <3arubin@gmail.com>
2020-03-02 02:44:21 +0300

    Module 7 task 2

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,22 @@
+const User = require('./../../models/User');
+
 module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+  if (!email) {
+    done(null, false, 'Не указан email');
+    return;
+  }
+  User.findOne({email: email}).then((user) => {
+    if (!user) {
+      user = new User({email, displayName});
+      user.save().then(
+          (result) => {
+            done(null, user);
+          },
+          (err) => {
+            done(err, false, err.errors.email.message);
+          });
+    } else {
+      done(null, user);
+    }
+  });
 };
