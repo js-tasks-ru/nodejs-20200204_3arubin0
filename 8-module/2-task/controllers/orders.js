cd97eb8c042a4c152124d5448b8dc96e24714289
3arubin <3arubin@gmail.com>
2020-03-04 22:33:49 +0300

    Module 8

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -1,8 +1,53 @@
+const mapOrder = require('../mappers/Order');
 const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  if (!ctx.user){
+    ctx.status = 401;
+    return;
+  }
+  const order = new Order({
+    user: ctx.user.id,
+    product: ctx.request.body.product,
+    phone: ctx.request.body.phone,
+    address: ctx.request.body.address,
+  });
+
+  let newOrder;
+  await order.save().then(
+      (result) =>{
+        newOrder = result;
+      },
+      (err) => {
+        const errResult = {};
+        if (err.errors.product) {
+          errResult.product = ['required'];
+        }
+        if (err.errors.phone) {
+          errResult.phone = err.errors.phone.message;
+        }
+        if (err.errors.address) {
+          errResult.address = ['required'];
+        }
+        ctx.status = 400;
+        ctx.body = {errors: errResult};
+        return;
+      }
+  );
+  if (newOrder) {
+    await sendMail({
+      template: 'order-confirmation',
+      locals: {id: newOrder.id, product: newOrder.product},
+      to: ctx.user.email,
+      subject: 'Order Registration',
+    });
+    ctx.body = {order: newOrder.id};
+  }
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const orders = await Order.find({user: ctx.user.id});
+  const orderMaps = orders.map((order) => mapOrder(order));
+  ctx.body = {orders: orderMaps};
 };
