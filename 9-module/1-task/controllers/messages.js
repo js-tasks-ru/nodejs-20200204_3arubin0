7c7da1035caefda3db0277c3646d5867cd678519
3arubin <3arubin@gmail.com>
2020-03-06 00:01:31 +0300

    module 9

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -1,5 +1,11 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  const messages = await Message.find({chat: ctx.user.id});
+
+  ctx.body = {messages: messages.map((msg)=>({
+    date: msg.date,
+    text: msg.text,
+    id: msg.id,
+    user: msg.user}))};
 };
